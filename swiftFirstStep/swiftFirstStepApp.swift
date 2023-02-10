1aaf72da2b28f2bb0701a576c03f81e99d990808
huseyin.sahinli <huseyin.sahinli@bitay.com>
2023-02-10 15:43:16 +0300

    Initial Commit

---
+++ swiftFirstStep/swiftFirstStepApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftFirstStepApp.swift
+//  swiftFirstStep
+//
+//  Created by Hüseyin ŞAHİNLİ on 10.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct swiftFirstStepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
